575be4f28b28ad4aab4dd72c80bc2949240ff99a
Rudolf Kolbe <rkolbe96@gmail.com>
2022-07-21 14:32:45 +0200

    update setup

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
-import os
-import re
-from setuptools import setup, find_packages, dist, Extension
+from setuptools import setup, find_packages, Extension
 
-# get the local path of the installation files
-LOCAL = os.path.dirname(os.path.realpath(__file__))
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 # options:
 extra_compile_args = [
+    # use Python's limited api for C++
+    # allows building wheels for all python (3) versions
+    "-DPy_LIMITED_API=0x03060000",
+    # fpng args
     "-fPIC",
     "-fno-strict-aliasing",
     "-D_LARGEFILE64_SOURCE=1",
@@ -24,7 +26,19 @@ setup(
     description="",
     author="K0lb3",
     version="0.0.1",
+    keywords=["png", "compression"],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Topic :: Multimedia :: Graphics",
+    ],
+    url="https://github.com/K0lb3/texture2ddecoder",
+    download_url="https://github.com/K0lb3/texture2ddecoder/tarball/master",
     packages=find_packages(),
+    long_description=long_description,
     long_description_content_type="text/markdown",
     install_requires=[],
     ext_modules=[
