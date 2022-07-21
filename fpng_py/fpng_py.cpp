729fd1460eaaec65ff5f7ab8e49d9bc491b3c293
Rudolf Kolbe <rkolbe96@gmail.com>
2022-07-21 14:33:26 +0200

    allow kwargs and improve hinting

--- fpng_py/fpng_py.cpp
+++ fpng_py/fpng_py.cpp
@@ -6,86 +6,124 @@ using namespace fpng;
 
 PyObject *py_fpng_cpu_supports_sse41(PyObject *self, PyObject *unused)
 {
-    PyObject* result = fpng_cpu_supports_sse41() ? Py_True : Py_False;
+    PyObject *result = fpng_cpu_supports_sse41() ? Py_True : Py_False;
     Py_INCREF(result);
     return result;
 }
 
-PyObject *py_fpng_crc32(PyObject *self, PyObject *args)
+PyObject *py_fpng_crc32(PyObject *self, PyObject *args, PyObject *kwargs)
 {
     const char *pData;
     Py_ssize_t size;
-    uint32_t prev_crc32;
-    if (!PyArg_ParseTuple(args, "y#i", &pData, &size, &prev_crc32))
+    uint32_t prev_crc32 = 0;
+
+    static char *kwlist[] = {
+        "data",
+        "prev_crc32",
+        NULL};
+
+    if (!PyArg_ParseTupleAndKeywords(args, kwargs, "y#|I", kwlist, &pData, &size, &prev_crc32))
     {
         PyErr_SetString(PyExc_ValueError, "Invalid arguments");
         return NULL;
     }
+
     return PyLong_FromUnsignedLong(fpng_crc32(pData, size, prev_crc32));
 }
 
-PyObject *py_fpng_adler32(PyObject *self, PyObject *args)
+PyObject *py_fpng_adler32(PyObject *self, PyObject *args, PyObject *kwargs)
 {
     const char *pData;
     Py_ssize_t size;
-    uint32_t adler;
-    if (!PyArg_ParseTuple(args, "y#i", &pData, &size, &adler))
+    uint32_t adler = 1;
+
+    static char *kwlist[] = {
+        "data",
+        "adler",
+        NULL};
+
+    if (!PyArg_ParseTupleAndKeywords(args, kwargs, "y#|I", kwlist, &pData, &size, &adler))
     {
         PyErr_SetString(PyExc_ValueError, "Invalid arguments");
         return NULL;
     }
+
     return PyLong_FromUnsignedLong(fpng_adler32(pData, size, adler));
 }
 
-PyObject *py_fpng_encode_image_to_memory(PyObject *self, PyObject *args)
+PyObject *py_fpng_encode_image_to_memory(PyObject *self, PyObject *args, PyObject *kwargs)
 {
-    const char *pImage;
+    char *pImage;
     Py_ssize_t size;
     uint32_t w, h;
     uint32_t num_chans = 0;
     uint32_t flags = 0;
-    if (!PyArg_ParseTuple(args, "y#II|II", &pImage, &size, &w, &h, &num_chans, &flags))
+
+    static char *kwlist[] = {
+        "image",
+        "width",
+        "height",
+        "num_channels",
+        "flags",
+        NULL};
+    if (!PyArg_ParseTupleAndKeywords(args, kwargs, "y#II|II", kwlist, &pImage, &size, &w, &h, &num_chans, &flags))
     {
         PyErr_SetString(PyExc_ValueError, "Invalid arguments");
         return NULL;
     }
+
     // calculate channels from size, making their input optional
     if (num_chans == 0)
     {
         num_chans = size / (w * h);
     }
+
     if (num_chans != 3 && num_chans != 4)
     {
         PyErr_SetString(PyExc_ValueError, "Invalid number of channels");
         return NULL;
     }
+
     if (flags > 2)
     {
         PyErr_SetString(PyExc_ValueError, "Invalid flags");
         return NULL;
     }
+
     std::vector<uint8_t> out_buf;
     if (!fpng_encode_image_to_memory(pImage, w, h, num_chans, out_buf, flags))
     {
         PyErr_SetString(PyExc_ValueError, "Error encoding image");
         return NULL;
     }
-    return PyBytes_FromStringAndSize((char*)out_buf.data(), out_buf.size());
+
+    return PyBytes_FromStringAndSize((char *)out_buf.data(), out_buf.size());
 }
 
-PyObject *py_fpng_encode_image_to_file(PyObject *self, PyObject *args)
+PyObject *py_fpng_encode_image_to_file(PyObject *self, PyObject *args, PyObject *kwargs)
 {
-    const char *pImage;
+    char *pImage;
     Py_ssize_t size;
     uint32_t w, h;
     uint32_t num_chans = 0;
     uint32_t flags = 0;
     const char *pFilename;
-    if (!PyArg_ParseTuple(args, "sy#II|II", &pFilename, &pImage, &size, &w, &h, &num_chans, &flags))
+
+    static char *kwlist[] = {
+        "filename",
+        "image",
+        "width",
+        "height",
+        "num_channels",
+        "flags",
+        NULL};
+
+    if (!PyArg_ParseTupleAndKeywords(args, kwargs, "sy#II|II", kwlist, &pFilename, &pImage, &size, &w, &h, &num_chans, &flags))
     {
         PyErr_SetString(PyExc_ValueError, "Invalid arguments");
         return NULL;
     }
+
     // calculate channels from size, making their input optional
     if (num_chans == 0)
     {
@@ -96,16 +134,19 @@ PyObject *py_fpng_encode_image_to_file(PyObject *self, PyObject *args)
         PyErr_SetString(PyExc_ValueError, "Invalid number of channels");
         return NULL;
     }
+
     if (flags > 2)
     {
         PyErr_SetString(PyExc_ValueError, "Invalid flags");
         return NULL;
     }
+
     if (!fpng_encode_image_to_file(pFilename, pImage, w, h, num_chans, flags))
     {
         PyErr_SetString(PyExc_ValueError, "Error encoding image");
         return NULL;
     }
+
     Py_RETURN_NONE;
 }
 
@@ -156,16 +197,22 @@ PyObject *py_decode_err(int code)
     return NULL;
 }
 
-PyObject *py_fpng_get_info(PyObject *self, PyObject *args)
+PyObject *py_fpng_get_info(PyObject *self, PyObject *args, PyObject *kwargs)
 {
     const char *pImage;
     Py_ssize_t size;
     uint32_t w, h, num_chans;
-    if (!PyArg_ParseTuple(args, "y#", &pImage, &size))
+
+    static char *kwlist[] = {
+        "image",
+        NULL};
+
+    if (!PyArg_ParseTupleAndKeywords(args, kwargs, "y#", kwlist, &pImage, &size))
     {
         PyErr_SetString(PyExc_ValueError, "Invalid arguments");
         return NULL;
     }
+
     int error = fpng_get_info(pImage, size, w, h, num_chans);
     if (error)
     {
@@ -174,12 +221,18 @@ PyObject *py_fpng_get_info(PyObject *self, PyObject *args)
     return Py_BuildValue("III", w, h, num_chans);
 }
 
-PyObject *py_fpng_decode_memory(PyObject *self, PyObject *args)
+PyObject *py_fpng_decode_memory(PyObject *self, PyObject *args, PyObject *kwargs)
 {
     const char *pImage;
     Py_ssize_t image_size;
     uint32_t desired_channels;
-    if (!PyArg_ParseTuple(args, "y#I", &pImage, &image_size, &desired_channels))
+
+    static char *kwlist[] = {
+        "image",
+        "desired_channels",
+        NULL};
+
+    if (!PyArg_ParseTupleAndKeywords(args, kwargs, "y#I", kwlist, &pImage, &image_size, &desired_channels))
     {
         PyErr_SetString(PyExc_ValueError, "Invalid arguments");
         return NULL;
@@ -190,55 +243,62 @@ PyObject *py_fpng_decode_memory(PyObject *self, PyObject *args)
     int error = fpng_decode_memory(pImage, image_size, out, width, height, channels_in_file, desired_channels);
     if (error)
         return py_decode_err(error);
-    return Py_BuildValue("y#III", (char*)out.data(), out.size(), width, height, channels_in_file);
+    return Py_BuildValue("y#III", (char *)out.data(), out.size(), width, height, channels_in_file);
 }
 
-PyObject *py_fpng_decode_file(PyObject *self, PyObject *args)
+PyObject *py_fpng_decode_file(PyObject *self, PyObject *args, PyObject *kwargs)
 {
     const char *pFilename;
     uint32_t desired_channels;
-    if (!PyArg_ParseTuple(args, "sI", &pFilename, &desired_channels))
+
+    static char *kwlist[] = {
+        "filename",
+        "desired_channels",
+        NULL};
+
+    if (!PyArg_ParseTupleAndKeywords(args, kwargs, "sI", kwlist, &pFilename, &desired_channels))
     {
         PyErr_SetString(PyExc_ValueError, "Invalid arguments");
         return NULL;
     }
+
     std::vector<uint8_t> out;
     uint32_t width, height, channels_in_file;
     int error = fpng_decode_file(pFilename, out, width, height, channels_in_file, desired_channels);
     if (error)
         return py_decode_err(error);
-    return Py_BuildValue("y#III", (char*)out.data(), out.size(), width, height, channels_in_file);
+    return Py_BuildValue("y#III", (char *)out.data(), out.size(), width, height, channels_in_file);
 }
 
 // Exported methods are collected in a table
 static struct PyMethodDef method_table[] = {
     {"fpng_cpu_supports_sse41",
      (PyCFunction)py_fpng_cpu_supports_sse41,
-     METH_VARARGS,
+     METH_NOARGS,
      ""},
     {"fpng_crc32",
      (PyCFunction)py_fpng_crc32,
-     METH_VARARGS,
+     METH_VARARGS | METH_KEYWORDS,
      ""},
     {"fpng_adler32",
      (PyCFunction)py_fpng_adler32,
-     METH_VARARGS,
+     METH_VARARGS | METH_KEYWORDS,
      ""},
     {"fpng_encode_image_to_memory",
      (PyCFunction)py_fpng_encode_image_to_memory,
-     METH_VARARGS,
+     METH_VARARGS | METH_KEYWORDS,
      ""},
     {"fpng_encode_image_to_file",
      (PyCFunction)py_fpng_encode_image_to_file,
-     METH_VARARGS,
+     METH_VARARGS | METH_KEYWORDS,
      ""},
     {"fpng_decode_from_memory",
      (PyCFunction)py_fpng_decode_memory,
-     METH_VARARGS,
+     METH_VARARGS | METH_KEYWORDS,
      ""},
     {"fpng_decode_from_file",
      (PyCFunction)py_fpng_decode_file,
-     METH_VARARGS,
+     METH_VARARGS | METH_KEYWORDS,
      ""},
     {NULL,
      NULL,
